13709795de8ce331631afd9d1c5fdba5e897de43
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-02-02 04:34:25 -0500

    Lists work

--- app.py
+++ app.py
@@ -14,10 +14,18 @@ def index():
 @app.route("/<username>", methods=['GET', 'POST'])
 def get_pastes(username):
   if request.method == 'GET':
-    return json.dumps(redis.lrange(username, 0, -1))
+    pastes = redis.lrange(username, 0, -1)
+    return json.dumps(pastes)
   else:
+    pastes = request.form.getlist('paste')
+    paste = request.form.get('paste')
     # handle arrays with *[]
-    return json.dumps(redis.lpush(username, request.form.get('paste')))
+    if pastes is not None:
+      return json.dumps(redis.lpush(username, *pastes))
+    elif paste is not None:
+      return json.dumps(redis.lpush(username, *paste))
+    else:
+      return ""
 
 if __name__ == "__main__":
   app.run(debug=True)
