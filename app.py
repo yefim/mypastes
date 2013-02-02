6e889504225bc02a9668aec7cc2b5a10e6c487a7
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-02-02 11:27:45 -0500

    responding to json properly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from redis import Redis
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 redis = Redis()
@@ -12,20 +12,22 @@ def index():
 
 
 @app.route("/<username>", methods=['GET', 'POST'])
-def get_pastes(username):
+def pastes(username):
   if request.method == 'GET':
     pastes = redis.lrange(username, 0, -1)
-    return json.dumps(pastes)
+    if request.headers['Accept'] == 'application/json':
+      return json.dumps(pastes)
+    else:
+      return render_template('paste.html', pastes=pastes)
   else:
     pastes = request.form.getlist('paste')
-    paste = request.form.get('paste')
-    # handle arrays with *[]
-    if pastes is not None:
-      return json.dumps(redis.lpush(username, *pastes))
-    elif paste is not None:
-      return json.dumps(redis.lpush(username, *paste))
+    # should check validity of pastes
+    redis.lpush(username, *pastes)
+    pastes = redis.lrange(username, 0, -1)
+    if request.headers['Accept'] == 'application/json':
+      return json.dumps(pastes)
     else:
-      return ""
+      return render_template('paste.html', pastes=pastes)
 
 if __name__ == "__main__":
   app.run(debug=True)
