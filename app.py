0f638125184c4919adbab58f8f8df4231eb38790
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-04-03 02:51:21 -0400

    Simplfied API

--- app.py
+++ app.py
@@ -11,25 +11,28 @@ def index():
   return render_template('index.html')
 
 
-@app.route("/<username>", methods=['GET', 'POST'], defaults={'number': 0})
-@app.route("/<username>/<int:number>", methods=['GET', 'POST'])
-def pastes(username, number):
+@app.route("/<username>", methods=['GET', 'POST'])
+def pastes(username):
   if request.method == 'GET':
     # -1 gets all the things
-    pastes = redis.lrange(username, 0, number - 1)
+    pastes = redis.lrange(username, 0, -1)
     if request.headers['Accept'] == 'application/json':
       return json.dumps(pastes)
     else:
-      return render_template('paste.html', pastes=pastes)
+      return render_template('paste.html',
+                             pastes=pastes,
+                             username=username)
   else:
     pastes = request.form.getlist('paste')
     # should check validity of pastes
     redis.lpush(username, *pastes)
-    pastes = redis.lrange(username, 0, number - 1)
+    all_pastes = redis.lrange(username, 0, -1)
     if request.headers['Accept'] == 'application/json':
-      return json.dumps(pastes)
+      return json.dumps(all_pastes)
     else:
-      return render_template('paste.html', pastes=pastes)
+      return render_template('paste.html',
+                             pastes=all_pastes,
+                             username=username)
 
 if __name__ == "__main__":
   app.run(debug=True)
